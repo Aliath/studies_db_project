e5900d1b5787550741c254aab01a46c5345cc658
Bartek Słysz <bartekslysz@poczta.fm>
2022-11-20 13:30:09 +0100

    Add procedure to dispose of "hate reviews"

---
+++ .tools/sql/procedure.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE delete_critical_reviews(rate_threshold integer)
+LANGUAGE SQL
+AS $$
+    DELETE FROM episode_reviews
+    WHERE
+        reviewer_id IN (SELECT users.id FROM episode_reviews LEFT JOIN users ON episode_reviews.reviewer_id = users.id GROUP BY users.id HAVING AVG(rate) < 3);
+$$;
